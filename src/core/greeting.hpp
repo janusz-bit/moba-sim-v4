b86ea0c5a8c21db53b382a51354cd4c8823f79fa
janusz-bit <janusz-bit@proton.me>
2026-08-05 06:38:08 +0200

    Remove greeting template leftover

--- src/core/greeting.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace moba_sim {
-
-[[nodiscard]] std::string greeting(const std::string& name);
-
-} // namespace moba_sim
--- tests/greeting.test.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include "core/greeting.hpp"
-
-TEST_CASE("greeting returns a personalized message", "[greeting]") {
-    REQUIRE(moba_sim::greeting("World") == "Hello, World!");
-    REQUIRE(moba_sim::greeting("moba-sim") == "Hello, moba-sim!");
-}
